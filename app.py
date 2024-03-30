66fe0e3e8aac9ba5067c07bf09d8295794dadfab
Kelly Cristina dos Santos <95941924+kellycsantos@users.noreply.github.com>
2024-03-30 18:30:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
 livros = [
     {
         "id": 0,
@@ -26,6 +28,38 @@ livros = [
     },
 ]
 
-@app.route('/')
+
 def get_livros():
     return jsonify(livros)
+
+@app.route('/desenhos')
+desenhosCartoonNetwork = [
+    {
+        id: 1,
+        nome: "Hora de Aventura",
+        anoLancamento: 2010
+    },
+    {
+        id: 2,
+        nome: "Steven Universo",
+        anoLancamento: 2013
+    },
+    {
+        id: 3,
+        nome: "O Incrível Mundo de Gumball",
+        anoLancamento: 2011
+    },
+    {
+        id: 4,
+        nome: "Apenas um Show",
+        anoLancamento: 2010
+    },
+    {
+        id: 5,
+        nome: "As Meninas Superpoderosas",
+        anoLancamento: 1998
+    }
+];
+
+def get_desenhos():
+    return jsonify(desenhosCartoonNetwork)
